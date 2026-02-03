228ca27d1fbb7b201345fd98511ff457ad9fc271
CallumJRobertson <90865822+CallumJRobertson@users.noreply.github.com>
2026-02-03 12:50:44 +0100

    Add files via upload

---
+++ Beamio/BeamioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamioApp.swift
+//  Beamio
+//
+//  Created by m1 on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BeamioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeamioTests/BeamioTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeamioTests.swift
+//  BeamioTests
+//
+//  Created by m1 on 03/02/2026.
+//
+
+import Testing
+@testable import Beamio
+
+struct BeamioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
